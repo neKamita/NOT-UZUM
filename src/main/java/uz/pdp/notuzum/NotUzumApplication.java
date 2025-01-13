cba599f3847cb6de771eb5f06c43933a11ed24d1
cdy <nationalUB@gmail.com>
2025-01-13 20:08:26 +0500

    First Commit

---
+++ src/main/java/uz/pdp/notuzum/NotUzumApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.notuzum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotUzumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotUzumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/notuzum/NotUzumApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.notuzum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotUzumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
